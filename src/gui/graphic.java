1cc66d7593aee16cdf8a12ee8f29e0856d0bcd5d
microbe <microbe@10.77.2.224>
2017-02-01 12:37:08 +0100

    test graphic

---
+++ src/gui/graphic.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.JFrame;
+
+public class graphic {
+	public static void main(String[] args) {
+	JFrame window = new JFrame() ;
+	window.setTitle("Test") ;
+	window.setVisible(true);
+	window.setSize(500,500);
+	window.setLocationRelativeTo(null);
+	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+}
