ab4a871f2d84a221b59621e19433cdba16d47a01
Valou95 <valentin95280@gmail.com>
2017-02-01 11:14:43 +0100

    init position

--- src/objects/Position.java
+++ src/objects/Position.java
@@ -1,5 +1,27 @@
 package objects;
 
 public class Position {
-
+	private int posX;
+	private int posY;
+	
+	public Position(int posX, int posY){
+		this.posX = posX;
+		this.posY = posY;
+	}
+	
+	public String getPosX(){
+		return posX;
+	}
+	
+	public String getPosY(){
+		return posY;
+	}
+	
+	public void setPosX(int posX){
+		this.posX;
+	}
+	
+	public void setPosY(int posY){
+		this.posY;
+	}
 }
