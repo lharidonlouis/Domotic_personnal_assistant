72df99973a2d8a5490fbf3418ecf37b15bbc4ab3
microbe <microbe@10.77.2.224>
2017-02-08 10:11:47 +0100

    creat switch case

--- src/objects/Status.java
+++ src/objects/Status.java
@@ -4,8 +4,27 @@ public class Status {
 	boolean on ;  
 	boolean detected ;
 	boolean problem ;
-	
+
 	public Status() {
+	}
+	
+	public void switchStatus (String var) {
+		switch (var) {
 		
+			case "on":
+				on= !on ;
+				break ; 
+				
+			case "detected" :
+				detected = !detected ;
+				break ;
+			case "problem" :
+				problem = !problem ;
+				break ;
+			
+			default :
+		}
 	}
 }
+
+
