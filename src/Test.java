46bcfc6d3981edca6d0a76bbc6b45947718dd24a
lharidonlouis <louislharidon56@gmail.com>
2017-02-08 11:23:39 +0100

    Remove comment

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,6 @@ import objects.Obj;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("test de val");
 		Obj objet = new Obj();
 	}
