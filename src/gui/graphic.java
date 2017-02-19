ce552e0baf3759ca66682a7fce9c2148974df035
microbe <microbe@10.0.0.7>
2017-02-19 12:37:02 +0100

    grid add

--- src/gui/graphic.java
+++ src/gui/graphic.java
@@ -1,15 +1,34 @@
  package gui;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
 
 public class graphic {
 	public static void main(String[] args) {
 	JFrame window = new JFrame() ;
-	window.setTitle("Test") ;
+	JPanel pan = new JPanel (new GridLayout (8,8));
+	
+	
+	window.setTitle("Grille") ;
 	window.setVisible(true);
 	window.setSize(500,500);
 	window.setLocationRelativeTo(null);
 	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+	
+	Border blackline = BorderFactory.createLineBorder(Color.black,1); 
+	for(int i = 0; i<64;i++){
+	   JPanel ptest = new JPanel();
+	   ptest.setBorder(blackline);
+	   pan.add(ptest);
+	}
+	
+	pan.setBorder(blackline);
+	window.add(pan) ;
 	}
 }
