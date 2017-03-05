fa1ba93c9b18fc31f097fc5abac1e098589da2f4
microbe <microbe@10.0.0.4>
2017-03-05 16:15:09 +0100

    grid house ok

--- src/gui/graphic.java
+++ src/gui/graphic.java
@@ -1,46 +1,116 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Frame;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.border.Border;
 
 
 public class graphic {
 	public static void main(String[] args) {
-		JFrame window = new JFrame() ;
-	
-		JPanel pan2 = new JPanel() ; 
-		JPanel panGrid= new JPanel( new GridLayout(20,20)) ;
 		
+		JFrame window = new JFrame() ;
+		JPanel pan2 = new JPanel() ; 
+		JPanel panGrid= new JPanel( new GridLayout(50,50)) ;
 		
 		window.setTitle("Grille") ;
 		window.setSize(1200,780);
 		window.setLocationRelativeTo(null);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 	
-	
-		for(int i = 0; i<(20*20);i++){
+		int tab[][] = new int [50][50] ; 
+		JPanel tabPanel[][] = new JPanel[50][50] ; 
 		
-		   Border blackline = BorderFactory.createLineBorder(Color.black,1); 
-		   JPanel panTest = new JPanel();
-		   panGrid.setBackground(Color.red);
-		   panTest.setBorder(blackline);
-		   panTest.setBackground(Color.blue);
-		   if(i==23*9){
-			   panTest.setBackground(Color.GREEN);
-		   }
-		   panGrid.setBorder(blackline);
-		   panGrid.add(panTest);
-		}		
+		// Initialisation of the grid
+		
+		for (int index1=0 ; index1<50; index1++ ){ 		
+			for (int index2=0 ; index2<50; index2++ ){
+				tab[index1][index2] = 0 ;
+			}
+		}
+		
+		// outside walls
+		
+		for (int i=0 ; i<50 ; i++) {
+			tab[i][0]=1 ;
+			tab[0][i]=1 ;
+			tab[i][49]=1 ;
+			tab[49][i]=1 ;
+		}
+		
+		// garage door and front door
+		
+		tab[49][5]=0 ;
+		tab[49][6]=0 ;
+		tab[49][7]=0 ;
+		tab[49][8]=0 ;
+		tab[49][9]=0 ;
+		tab[49][10]=0 ;
+		
+		tab[49][25]=0 ;
+		tab[49][26]=0 ;
+		tab[49][27]=0 ;
+		
+		// walls house 
+		for (int i=0 ; i<50 ; i++) {
+			tab[i][15]=1 ;
+			
+		}
+		
+		// house's door
+		
+		tab[5][15]=0 ;
+		tab[6][15]=0 ;
+		tab[7][15]=0 ;
+		tab[25][15]=0 ;
+		tab[26][15]=0 ;
+		tab[27][15]=0 ;
+		tab[40][15]=0 ;
+		tab[41][15]=0 ;
+		tab[42][15]=0 ;
+		
+		// Inside walls
+		
+		for (int i=0 ; i<15 ; i++) {
+			tab[13][i]=1 ;
+			tab[29][i]=1 ;			
+		}
+		
+		// creat grid display
+		
+		for (int index1=0 ; index1<50; index1++ ){
+			for (int index2=0 ; index2<50; index2++ ){
+				if (tab[index1][index2] == 1) {
+					JPanel wall = new JPanel() ;
+					wall.setBackground(Color.BLACK);
+					panGrid.add(wall);
+				}
+				else {
+					JPanel j = new JPanel() ;
+					j.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+					panGrid.add(j) ;
+				}
+				
+			}
+		}
+		
+		for (int index1=0 ; index1<50; index1++ ){
+			for (int index2=0 ; index2<50; index2++ ){
+					System.out.println(tab[index1][index2]);
+			}
+		}
 		panGrid.setSize(800, 760);
+		pan2.setSize(400,760);
 		window.add(panGrid) ;
-		window.add(pan2);	
+		window.add(pan2);
 		window.setVisible(true);
-		//TEST
-	}		
+	
+		
+	}	
 }
