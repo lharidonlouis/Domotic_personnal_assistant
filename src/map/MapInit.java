1518525a4628b48aea2eabf0589aacc69f6310b2
JulienDMT <julien.demoineret@gmail.com>
2017-03-27 20:47:22 +0200

    map init red case

--- src/map/MapInit.java
+++ src/map/MapInit.java
@@ -1,6 +1,11 @@
 package map;
 
-import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+import objects.Obj;
 
 public class MapInit{
 	protected int tab[][] = new int [50][50];
@@ -25,45 +30,70 @@ public class MapInit{
 		}
 			
 			// garage door and front door
-		for (int index=5 ; index<=10 ; index++) {
+		for (int index=6 ; index<=16 ; index++) {
 			tab[49][index]=0 ;
 		}
-		for (int index=25 ; index<=27 ; index++) {
+		for (int index=34 ; index<=37 ; index++) {
 			tab[49][index]=0 ;
 		}
 		
 		// walls house 
 		for (int i=0 ; i<50 ; i++) {
-			tab[i][15]=1 ;
+			tab[i][22]=1 ;
 		}
 			
 		// house's door
 		for (int i=5 ; i<=7 ; i++) {
-			tab[i][15]=0 ;
+			tab[i][22]=0 ;
 		}
 		
 		for (int i=25 ; i<=27 ; i++) {
-			tab[i][15]=0 ;
+			tab[i][22]=0 ;
 		}
 		
 		for (int i=40 ; i<=42 ; i++) {
-			tab[i][15]=0 ;
+			tab[i][22]=0 ;
 		}
 		
-			
+	
 		// Inside walls
 			
 
-		for (int i=0 ; i<15 ; i++) {
+		for (int i=0 ; i<22 ; i++) {
 			tab[13][i]=1 ;
 			tab[29][i]=1 ;	
 		}
 
 		// Inside walls
 			
-		for (int i=0 ; i<15 ; i++) {
+		for (int i=0 ; i<22 ; i++) {
 			tab[13][i]=1 ;
 			tab[29][i]=1 ;			
-		}		
+		}	
+		
+		tab[10][10]=2 ;
 	}
+	
+	protected void reader() {
+		String csvFile = "/log.csv";
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] data = line.split(cvsSplitBy);
+
+                System.out.println("Country [code= " + data[4] + " , name=" + data[5] + "]");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
